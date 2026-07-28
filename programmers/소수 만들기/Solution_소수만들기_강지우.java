7d138ab84630dd7f5ceeae3180550b4fa2859b22
강지우 <w4762@naver.com>
2026-07-29 00:22:53 +0900

    Upload New File

---
+++ programmers/소수 만들기/Solution_소수만들기_강지우.java
@@ -0,0 +1,34 @@
+class Solution_소수만들기_강지우 {
+	public int solution(int[] nums) {
+        int count = 0;
+
+        for (int i=0; i<nums.length-2; i++) {
+            for (int j=i+1; j<nums.length-1; j++) {
+                for (int k=j+1; k<nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+
+
+                    if (isPrime(sum)) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+	    return count;
+	}
+	
+	private boolean isPrime(int n) {
+	    if(n<2) {
+	        return false;
+	    }
+	    
+	    for (int i=2; i*i<=n; i++) {
+	        if(n%i==0) {
+	            return false;
+	        }
+	    }
+	    return true;
+	    
+	}
+}
