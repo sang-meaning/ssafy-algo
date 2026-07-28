e11dbb0b52429c171a6c7ce48ab86a3814ac2913
이윤찬 <dbscksaos@naver.com>
2026-07-28 15:05:18 +0900

    Upload New File

---
+++ programmers/소수찾기/Solution_소수찾기_이윤찬.java
@@ -0,0 +1,22 @@
+
+public class Solution {
+	public int solution(int n) {
+		int answer = 0;
+		for (int i = 2; i <= n; i++) {
+			if (isprime(i)) {
+				answer++;
+			}
+		}
+		return answer;
+
+	}
+
+	public boolean isprime(int n) {
+		for (int i = 2; i * i <= n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
