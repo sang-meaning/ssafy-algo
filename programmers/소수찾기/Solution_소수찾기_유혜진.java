ecbe9933a373c3e8c328bd1c584500ad6c76dcfd
유혜진 <hyejinyu578@gmail.com>
2026-07-28 14:14:32 +0900

    Upload New File

---
+++ programmers/소수찾기/Solution_소수찾기_유혜진.java
@@ -0,0 +1,37 @@
+package com.ssafy.ws.step3;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		for(int i = 0; i<=N; i++) {
+			make_prime(i);
+		}
+	}
+
+	private static void make_prime(int number) {
+		if(number < 2) {
+			return;
+		}
+		
+		if(number == 2) {
+			System.out.println(number);
+			return;
+		}
+		
+		for(int i=2; i<number; i++) {
+			if(number%i == 0) {
+				return;
+			}
+		}
+		
+		System.out.println(number);
+		return;
+	}
+	
+}
