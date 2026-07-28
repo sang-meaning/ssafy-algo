f1b383d41342919b753fdacac121aae5315dfd67
홍성대 <tjdoe5085@naver.com>
2026-07-28 14:06:52 +0900

    Upload New File

---
+++ programmers/소수찾기/Solution_소수찾기_홍성대.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    public int solution(int n) {
+        int answer = 0;
+        boolean [] check = new boolean[n+1];
+        check[0] = check[1] = true;
+        for(int i = 2; i*i<n+1; i++){
+            if(check[i] == true){
+                continue;
+            }
+            for(int j=i*i; j<n+1; j +=i){
+                check[j] = true;
+            }
+        }
+        for(int i = 2; i < n+1; i++){
+            if(check[i] == false){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
