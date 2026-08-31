8f49465cec4fdf0913cdf67dfe65186c738fa68d
박건순 <98qkrrjstns@naver.com>
2026-08-31 13:52:13 +0900

    Add new file

---
+++ SWEA/3260_두수의 덧셈/Solution_3260_박건순.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.math.BigInteger;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			BigInteger A = new BigInteger(sc.next());
+            BigInteger B = new BigInteger(sc.next());
+            
+            System.out.println("#"+ test_case + " "+ (A.add(B)));
+		}
+	}
+}
