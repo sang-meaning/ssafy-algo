4777f380fd8b5314920671be4efcfedf12481485
윤석웅 <letsgo125@naver.com>
2026-09-01 20:27:08 +0900

    Edit Solution_3499_윤석웅.java (add annotation)

--- SWEA/3499_퍼펙트 셔플/Solution_3499_윤석웅.java
+++ SWEA/3499_퍼펙트 셔플/Solution_3499_윤석웅.java
@@ -9,23 +9,23 @@ class Solution{
         int T = sc.nextInt();
         for (int test_case =1; test_case<=T; test_case++){
             Deque<String> que1 = new ArrayDeque<>();
-            Deque<String> que2 = new ArrayDeque<>();
+            Deque<String> que2 = new ArrayDeque<>(); // 큐 생성 두개만
             int samples= sc.nextInt();
-            for (int i=0;i<(samples+1)/2;i++){
+            for (int i=0;i<(samples+1)/2;i++){ // 앞 반수 +1(홀수인 경우)
                 que1.offer(sc.next());
             }
-            for (int i=0;i<samples/2;i++){
+            for (int i=0;i<samples/2;i++){ // 뒤 반수 (홀수에도 앞보다 1 적은 수)
                 que2.offer(sc.next());
             }
 
-            sb.append("#").append(test_case).append(" ");
-            for (int i = 0; i<samples;i++){
-                if (i%2==0){
+            sb.append("#").append(test_case).append(" "); // 테케 문자 입력
+            for (int i = 0; i<samples;i++){ // StringBuilder에 바로 쌓기
+                if (i%2==0){ // que1이 앞에 오므로
                     sb.append(que1.poll());
                 }else{
                     sb.append(que2.poll());
                 }
-                if(i!=samples-1){
+                if(i!=samples-1){ // 뒤에 띄어쓰기 방지
                     sb.append(" ");
                 }
             }
