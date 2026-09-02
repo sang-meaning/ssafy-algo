95fcfd91d5295c7da75e15134fae30d1fee6a214
김현호 <hno130@naver.com>
2026-09-02 17:39:02 +0900

    Edit Solution_연속부분수열합의개수_김현호.java

--- programmers/연속 부분 수열 합의 개수/Solution_연속부분수열합의개수_김현호.java
+++ programmers/연속 부분 수열 합의 개수/Solution_연속부분수열합의개수_김현호.java
@@ -3,18 +3,19 @@ class Solution {
     public int solution(int[] elements) {
         int answer = 0;
         int size = elements.length;
-        int[] double_elem = new int[size*2];
         Set<Integer> setsum = new HashSet<>();
-        for(int i = 0; i < size*2; i++){
-            double_elem[i] = elements[i%size];
-        }
-        System.out.println(double_elem.length);
-        for(int i = 1; i <= size; i++){
-            for(int j = 0; j < size; j++){
-                int sum = 0;
-                for(int k = j; k < j + i; k++){
-                    sum += double_elem[k];
-                }
+        for(int len = 1; len <= size; len++){
+            int sum = 0;
+            for(int i = 0; i < len; i++){
+                sum += elements[i];
+            }
+            setsum.add(sum);
+            for(int n = 1; n < size; n++){
+                int out = n - 1;
+                int in = (len + n - 1) % size;
+                
+                sum -= elements[out];
+                sum += elements[in];
                 setsum.add(sum);
             }
         }
