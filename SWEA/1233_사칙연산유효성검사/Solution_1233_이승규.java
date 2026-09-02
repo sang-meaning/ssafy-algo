5eec787cabf0bb3f161683d4f6d709f06ace606f
이승규 <sus_111@naver.com>
2026-09-02 14:39:40 +0900

    Solution_1233_이승규.java

--- SWEA/1233_사칙연산유효성검사/Solution_1233_이승규.java
+++ SWEA/1233_사칙연산유효성검사/Solution_1233_이승규.java
@@ -1,33 +1,8 @@
 package coding_prac;
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-//기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-//아래 표준 입출력 예제 필요시 참고하세요.
-//표준 입력 예제
-//int a;
-//double b;
-//char g;
-//String var;
-//long AB;
-//a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-//b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-//g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-//var = sc.next();                            // 문자열 1개 입력받는 예제
-//AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-//표준 출력 예제
-//int a = 0;                            
-//double b = 1.0;               
-//char g = 'b';
-//String var = "ABCDEFG";
-//long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /*
 사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
@@ -46,6 +21,7 @@ class Solution_1233_이승규 {
 		/*
 		 * 표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
 		 */
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int T;
 		T = 10;
 		/*
@@ -53,7 +29,31 @@ class Solution_1233_이승규 {
 		 */
 
 		for (int test_case = 1; test_case <= T; test_case++) {
+			// 자식이 있다면... 연산자
+			// 자식이 없다면... 숫자
+			int n = Integer.parseInt(bf.readLine());
+			int answer = 1;
 			
+			for(int i = 0; i < n; i++) {
+				String s = bf.readLine();
+				StringTokenizer st = new StringTokenizer(s);
+				int cnt = st.countTokens();
+				st.nextToken();
+				String target = st.nextToken();
+				
+				if(target.equals("+") || target.equals("-") || target.equals("*") || target.equals("/")) {
+					if(cnt != 4) {
+						answer = 0;
+						break;
+					}
+				} else {
+					if(cnt != 2) {
+						answer = 0;
+						break;
+					}
+				}
+			} 
+			System.out.println("#" + test_case + " " + answer);
 		}
 	}
 }
