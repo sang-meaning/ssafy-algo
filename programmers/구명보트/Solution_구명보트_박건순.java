311b73b23ddcdfff652f7aa4632a01fcb6e1b29e
박건순 <98qkrrjstns@naver.com>
2026-09-02 15:34:47 +0900

    Add new file

---
+++ programmers/구명보트/Solution_구명보트_박건순.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length-1;
+        
+        while(left<=right){
+            answer += 1;
+        
+            if (people[left] + people[right] <= limit){
+                left += 1;
+                right -= 1;
+            }else{
+                right -= 1;
+            }
+                
+        }
+        return answer;
+    }
+}
