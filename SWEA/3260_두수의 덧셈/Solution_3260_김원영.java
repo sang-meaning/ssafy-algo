64c5824c9adb16e5a96cf0a506fa885cda5d875f
김원영 <emforhsdnjsd@naver.com>
2026-09-02 09:09:36 +0900

    Edit Solution_3260_김원영.java

--- SWEA/3260_두수의 덧셈/Solution_3260_김원영.java
+++ SWEA/3260_두수의 덧셈/Solution_3260_김원영.java
@@ -1,8 +1,8 @@
 public class Solution {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(new File("sample_input.txt"));
+        Scanner sc = new Scanner(System.in);
 
         int T = sc.nextInt();
 
