caceeda08a772b52b7598c86977798d5986311e7
김원영 <emforhsdnjsd@naver.com>
2026-09-02 10:36:33 +0900

    Edit Solution_1225_김원영.java

--- SWEA/1225_암호생성기/Solution_1225_김원영.java
+++ SWEA/1225_암호생성기/Solution_1225_김원영.java
@@ -1,61 +1,43 @@
+import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Solution {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-
-        for(int tc=1; tc<=T; tc++) {
-
-            String A = sc.next();
-            String B = sc.next();
-
-            int a = A.length();
-            int b = B.length();                      
-
-            int c = (a >= b ? a : b) + 1;
-
-            int [] Aarray = new int[c-1];
-            for(int i=0; i<a; i++) {
-                Aarray[(c-1-a)+i] = A.charAt(i) - '0';
-            }
-
-            int [] Barray = new int[c-1];
-            for(int i=0; i<b; i++) {
-                Barray[(c-1-b)+i] = B.charAt(i) - '0';
-            }
-
-            int [] Carryarray = new int[c];
-            int [] Answerarray = new int[c];
-
-
-            for(int i=c-2; i>=0; i--) {
-                int temp = Aarray[i] + Barray[i] + Carryarray[i+1];
-                if(temp >= 10) {
-                    Carryarray[i] = 1;
-                    Answerarray[i+1] = temp % 10;
-                } else {
-                    Answerarray[i+1] = temp;
-                }
-            }
-
-            if(Carryarray[0] != 0) {
-                Answerarray[0] = Carryarray[0];
-            }
-
-            StringBuilder sb = new StringBuilder();
-
-            int start = 0;
-            if(Answerarray[0] == 0) {
-                start = 1;
-            }
-
-            for(int i=start; i<c; i++) {
-                sb.append(Answerarray[i]);
-            }
-
-            System.out.println("#" + tc + " " + sb);
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = 10;
+		
+		for(int i = 0; i < T ; i++) {
+			int tc = sc.nextInt();
+			Queue<Integer> queue = new ArrayDeque<>();
+			
+			for(int j=0; j<8;j++) {
+				queue.offer(sc.nextInt());
+			}
+			while(true) {
+				boolean end = false;
+				for(int k=1; k<=5; k++) {
+					if((queue.peek()-k) <= 0) {
+						queue.offer(0);
+						queue.poll();
+						end = true;
+						break;
+					}else {
+						queue.offer(queue.peek()-k);
+						queue.poll();
+					}
+				}
+				if(end) {
+					break;
+				}
+			}
+			System.out.print("#"+tc+" ");
+			for(int nums : queue) {
+				System.out.print(nums+" ");
+			}
+			System.out.println();
+		}
+		
+	}
 }
