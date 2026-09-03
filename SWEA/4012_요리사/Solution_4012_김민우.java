9b91086e0aab22409dbe6574a5aad21743e91aa2
김민우 <minwooxox@naver.com>
2026-09-03 14:17:20 +0900

    Update file Solution_4012_김민우.java

--- SWEA/4012_요리사/Solution_4012_김민우.java
+++ SWEA/4012_요리사/Solution_4012_김민우.java
@@ -5,7 +5,7 @@ public class Solution_4012_김민우 {
   static int[][] ingred;
   static int T, N, min;
   static List<Integer> listA;
-  static List<Integer> listAll;
+  static List<Integer> listB;
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,11 +17,14 @@ public class Solution_4012_김민우 {
       N = Integer.parseInt(st.nextToken());
       ingred = new int[N][N];
 
+      // A, B 음식에 사용할 재료 리스트 
       listA = new ArrayList<>();
-      listAll = new ArrayList<>();
+      listB = new ArrayList<>();
 
       for(int i = 0; i < N; i++){
-        listAll.add(i);
+        //처음 초기화할 때는 B음식(=listB)쪽에 모든 재료를 할당
+        //이후 A음식(=listA)에 재료들을 할당하면서, 이를 B음식(=listB)으로부터 제거할 예정
+        listB.add(i);
         st = new StringTokenizer(br.readLine());
         for(int j = 0; j < N; j++){
           ingred[i][j] = Integer.parseInt(st.nextToken());
@@ -36,11 +39,13 @@ public class Solution_4012_김민우 {
   }//main 끝
 
   public static void dfs(int idx, int cnt){
+    //A음식이 사용하는 재료 개수(=cnt)가 N/2일 때
+    //각 음식의 시너지 및 차이 계산 => 이후 최소값 갱신
     if(cnt == N/2){
       System.out.println(listA);
-      System.out.println(listAll);
+      System.out.println(listB);
       int foodA = cook(listA);
-      int foodB = cook(listAll);
+      int foodB = cook(listB);
       int diff = Math.abs(foodA-foodB);
       min = (diff < min)?diff:min;
       return;
@@ -49,13 +54,21 @@ public class Solution_4012_김민우 {
     else if(idx == N-1 && cnt != N/2){
       return;
     }
-    //포함 했을 때
+
+    //0~N-1번째 재료까지, A음식(=listA)이 해당 재료를 사용할 때와 사용하지 않을 때를 나눠 dfs 재귀호출
+
+    //1.
+    //A음식(=listA)이 idx+1번째 재료를 사용한다 했을 때
+    //B음식(=listB)은 해당 재료를 사용하지 못하므로 remove를 통해 제거
     listA.add(idx+1);
-    listAll.remove(Integer.valueOf(idx+1));
+    listB.remove(Integer.valueOf(idx+1));
     dfs(idx+1, cnt+1);
-    //포함 안했을 때
+    
+    //2.
+    //A음식(=listA)이 idx+!번째 재료를 사용하지 않는다 했을 때,
+    //앞서 A음식(=listA)에 포함됐었던 해당 재료를 다시 제거한 뒤, B음식(=listB)에 다시 할당
     listA.remove(Integer.valueOf(idx+1));
-    listAll.add(idx+1);
+    listB.add(idx+1);
     dfs(idx+1, cnt);
   }
 
