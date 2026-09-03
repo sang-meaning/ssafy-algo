db727fa5151670f201e2242d3e4e7c5acada312b
박건순 <98qkrrjstns@naver.com>
2026-09-03 10:38:22 +0900

    Add new file

---
+++ programmers/연속된 부분 수열의 합/Solution_연속된부분수열의합 _박건순.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[2];
+
+        int left = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        for (int right = 0; right < sequence.length; right++) {
+            sum += sequence[right];
+
+            while (sum > k) {
+                sum -= sequence[left++];
+            }
+
+            if (sum == k) {
+                int length = right - left + 1;
+
+                if (length < minLength) {
+                    answer[0] = left;
+                    answer[1] = right;
+                    minLength = length;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
