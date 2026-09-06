63c094b7eac4bbc91df3d4e119026b9cd795fdfb
정민수 <kinggodminsu@naver.com>
2026-09-06 23:06:17 +0900

    Add new file

---
+++ programmers/타겟넘버/Solution_타켓넘버_정민수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = dfs(numbers, target, 0, 0);
+        return answer;
+    }
+
+    private int dfs(int[] numbers, int target, int index, int sum) {
+        if (index == numbers.length) {
+            return sum == target ? 1 : 0;
+        }
+        
+        return dfs(numbers, target, index + 1, sum + numbers[index]) 
+             + dfs(numbers, target, index + 1, sum - numbers[index]);
+    }
+}
