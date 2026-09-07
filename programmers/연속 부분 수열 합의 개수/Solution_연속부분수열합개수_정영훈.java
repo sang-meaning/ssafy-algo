9f5ba2acb5323ecbffd88e14eb24b5bae7437d2f
JYH <wjddudgns0918@gmail.com>
2026-09-07 16:27:19 +0900

    Solution_연속부분수열합개수_정영훈

---
+++ programmers/연속 부분 수열 합의 개수/Solution_연속부분수열합개수_정영훈.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+class Solution_연속부분수열합개수_정영훈{
+    public int solution(int[] elements) {
+        int answer = 0;
+        int length = elements.length;
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = 1; i <= length; i++){
+            int left = 0;
+            int right = left + i;
+            int sum = 0;
+            
+            for(int j = left; j<right; j++){
+                sum += elements[j];
+            }
+            
+            while(left < length){
+                set.add(sum);
+                if(length == i) break;
+                
+                right %= length;
+                sum -= elements[left++];
+                sum += elements[right++];
+            }
+        }
+        answer = set.size();
+        return answer;
+    }
+}
