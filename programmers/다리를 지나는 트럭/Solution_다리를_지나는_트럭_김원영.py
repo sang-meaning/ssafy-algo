a457b8a542b6711d0fee3dafc8e16da8662efebb
김원영 <emforhsdnjsd@naver.com>
2026-09-07 14:23:51 +0900

    Edit Solution_다리를_지나는_트럭_김원영.py

--- programmers/다리를 지나는 트럭/Solution_다리를_지나는_트럭_김원영.py
+++ programmers/다리를 지나는 트럭/Solution_다리를_지나는_트럭_김원영.py
@@ -1,23 +1,31 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    wait = deque(truck_weights)
+    after = []
+    goal = len(truck_weights)
     bridge = deque()
+    before = deque(truck_weights)
     time = 0
-    sum_weight = 0
+    total_weight = 0
+    
+    for i in range(bridge_length):
+        bridge.append(0)
 
-    while wait or bridge:
+    while len(after) != goal:
         time += 1
-        if bridge and time - bridge[0][1] == bridge_length:
-            truck = bridge.popleft()
-            sum_weight -= truck[0]
+        
+        if bridge and bridge[0] != 0:
+            after.append(bridge[0])
+            total_weight -= bridge[0]
             
-        if wait and sum_weight + wait[0] <= weight:
-            truck = wait.popleft()
-            bridge.append((truck, time))
-            sum_weight += truck
+        bridge.popleft()
+        bridge.append(0)
+        
+        if before and total_weight + before[0] <= weight:
+            bridge[-1] = before[0]
+            before.popleft()
+            total_weight += bridge[-1]
+        else:
+            continue
 
-    answer = time
-
-    return answer
+    return time
