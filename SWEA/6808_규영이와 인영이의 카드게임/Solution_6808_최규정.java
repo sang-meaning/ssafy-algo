3d076168d60115993a149c55ba7ab2d268352019
최규정 <s1204x@gmail.com>
2026-09-07 12:10:11 +0900

    Update file Solution_6808_최규정.java

--- SWEA/6808_규영이와 인영이의 카드게임/Solution_6808_최규정.java
+++ SWEA/6808_규영이와 인영이의 카드게임/Solution_6808_최규정.java
@@ -14,7 +14,7 @@ public class Solution {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-int T = sc.nextInt();
+        int T = sc.nextInt();
 
         for (int test_case = 1; test_case <= T; test_case++) {
 
