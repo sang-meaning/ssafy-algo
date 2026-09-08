60451d39eb17b1d5b5fdeb3d597a682c8b74e4d6
이승규 <sus_111@naver.com>
2026-09-08 16:39:53 +0900

    Solution_3421_이승규

---
+++ SWEA/3421_수제 버거 장인/Solution_3421_이승규.java
@@ -0,0 +1,84 @@
+package alg_prac;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/*
+사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+*/
+class Solution_3421_이승규 {
+	static int n, m, answer;
+	static int[] badIngA, badIngB;
+	static ArrayList<Integer> burger;
+
+	public static void main(String args[]) throws Exception {
+		/*
+		 * 아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다. 여러분이 작성한 코드를
+		 * 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후, 이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때
+		 * 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다. 따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		 * 단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+//System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		 * 표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(bf.readLine());
+		/*
+		 * 여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		 */
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			String s = bf.readLine();
+			StringTokenizer st = new StringTokenizer(s);
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+
+			badIngA = new int[m];
+			badIngB = new int[m];
+			for (int i = 0; i < m; i++) {
+				s = bf.readLine();
+				st = new StringTokenizer(s);
+				badIngA[i] = Integer.parseInt(st.nextToken());
+				badIngB[i] = Integer.parseInt(st.nextToken());
+			}
+
+			burger = new ArrayList<>();
+			answer = 0;
+			dfs(0);
+
+			System.out.println("#" + test_case + " " + answer);
+		}
+	}
+
+	public static void dfs(int depth) {
+		if (depth == n) {
+			if (check())
+				answer++;
+			return;
+		}
+
+		// 안넣은경우
+		dfs(depth + 1);
+
+		// 넣은경우
+		burger.add(depth + 1);
+		dfs(depth + 1);
+
+		burger.remove(burger.size() - 1);
+	}
+
+	public static boolean check() {
+		for (int i = 0; i < m; i++) {
+			if (burger.contains(badIngA[i]) && burger.contains(badIngB[i]))
+				return false;
+		}
+
+		return true;
+	}
+
+}
