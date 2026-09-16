e248b43c67849c8896ffc96371cafdceea20f79f
최규정 <s1204x@gmail.com>
2026-09-16 10:40:21 +0900

    Add new file

---
+++ SWEA/1227_미로2/Solution_1227_최규정.java
@@ -0,0 +1,65 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		 Scanner sc = new Scanner(System.in);
+
+
+			for(int T = 0; T < 10; T++)
+			{
+                int test_case = sc.nextInt();
+                
+				int[][] map = new int[100][100];
+				boolean[][] visited = new boolean[100][100];
+				Queue<int[]> queue = new ArrayDeque<>();
+				int[] dx = {1,-1,0,0};
+				int[] dy = {0,0,-1,1};
+				int answer = 0;
+				  
+				
+				for(int i = 0; i < 100; i++) {
+					String line = sc.next();
+					for(int j = 0; j < 100; j++) {
+						map[i][j] = line.charAt(j) - '0';
+						
+						if(map[i][j] == 2) {
+							queue.offer(new int[] {i,j});
+							visited[i][j] =  true;
+						}
+					}
+				}				
+				
+				while(!queue.isEmpty() && answer == 0) {
+					int[] now = queue.poll();
+					
+					int x = now[0];
+					int y = now[1];
+					
+					
+					
+					for(int i = 0; i < 4; i++) {
+						int nx = x + dx[i];
+						int ny = y + dy[i];
+						
+						if(nx < 0 || ny < 0 || nx > 99 || ny > 99|| visited[nx][ny] == true || map[nx][ny] == 1) {
+							continue;
+						}
+
+						if(map[nx][ny] == 3) {
+							answer = 1;
+							break;
+						}
+						
+						visited[nx][ny] = true;
+						queue.offer(new int[] {nx,ny});
+		
+				}
+			}
+				System.out.println("#" + test_case + " " + answer);
+		}
+	}
+}
