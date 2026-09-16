e7ba870471634de7e77002947fbbd577a5eccd50
김정원 <comkjo0512@gmail.com>
2026-09-16 15:24:16 +0900

    Edit Solution_3421_김정원.java

--- SWEA/3421_수제 버거 장인/Solution_3421_김정원.java
+++ SWEA/3421_수제 버거 장인/Solution_3421_김정원.java
@@ -1,78 +1,69 @@
-package com.ssafy.swea;
+package ssafy.swea.kjw;
 
 import java.io.*;
 import java.util.*;
 
 public class Solution_3421_김정원 {
-    static int n, m;
-    static boolean[][] bad;
-    static boolean[] selected;
-    static int answer;
-
-    public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int T = Integer.parseInt(br.readLine());
-        for (int test_case = 1; test_case <= T; test_case++) {
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-
-            bad = new boolean[n + 1][n + 1];
-            selected = new boolean[n + 1];
-            answer = 0;
-
-            // 같이 들어가면 안되는 재료를 입력 받아서 저장한다
-            for (int i = 0; i < m; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                bad[a][b] = true;
-                bad[b][a] = true;
-            }
-
-            dfs(1);
-
-            System.out.println("#" + test_case + " " + answer);
-        }
-    }
-
-    static void dfs(int idx) {
-        // 모든 재료의 선택 여부를 정했다면 버거의 종류를 증가
-        // 재료를 하나도 넣지 않은 경우도 포함됨
-        if (idx > n) {
-            answer++;
-            return;
-        }
-
-        // 재료 선택에서 나올 수 있는 모든 경우의수를 확인한다
-        // 먼저 현재 재료를 넣지 않는 경우
-        dfs(idx + 1);
-
-        // 현재 선택한 재료들과 같이 넣을 수 있는지 확인
-        if (!check(idx)) return;
-
-        // 현재 재료를 넣는 경우
-        selected[idx] = true;
-        dfs(idx + 1);
-
-        // 다른 경우의수를 확인하기 위해서 선택을 해제한다
-        selected[idx] = false;
-    }
-
-    static boolean check(int idx) {
-        // 현재 재료 이전까지 선택한 재료들을 확인한다
-        for (int i = 1; i < idx; i++) {
-            // 선택한 재료가 아니라면 넘어간다
-            if (!selected[i]) continue;
-
-            // 같이 들어가면 안되는 재료라면 리턴하여 종료
-            if (bad[idx][i]) return false;
-        }
-
-        return true;
-    }
+	static int N,M,target,answer;
+	static Map<Integer, Set<Integer>> ban;
+	static boolean[] selected;
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			ban = new HashMap<>();
+			answer = 0;
+			selected = new boolean[N + 1];
+			// 동시에 사용할 수 없는 재료 쌍을 입력 받기
+			// 예를 들어 1번 재료가 어떤게 동시에 사용 못하는지 알고 싶다면
+			// ban.get(1) 하면 boolean[] 를 반환해주니 거기서 false 인것만 골라오면 된다
+			for (int i = 1; i <= N;i++) {
+				ban.put(i, new HashSet<>());
+			}
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int key = Integer.parseInt(st.nextToken());
+				int ingredient = Integer.parseInt(st.nextToken());
+				ban.get(key).add(ingredient);
+				ban.get(ingredient).add(key);
+			}
+			cook(1, new HashSet<>());
+			System.out.println(String.format("#%d %d", test_case, answer));
+		}
+	}
+	
+	static void cook(int cnt, Set<Integer> ban_list) {
+		// 모든 식재료를 선택할지 말지 정했다면
+		if (cnt > N) {
+			answer++;
+			return;
+		}
+		
+		// 현재 재료가 금지된 식재료가 아니라면 선택 가능
+		if (!ban_list.contains(cnt)) {
+			selected[cnt] = true;
+			Set<Integer> next_ban_list = new HashSet<>(ban_list);
+			// 현재 선택한 식재료와 같이 사용하지 못하는 식재료들을 금지 목록에 추가
+			next_ban_list = banIngredinets(cnt, next_ban_list);
+			cook(cnt + 1, next_ban_list);
+			// 돌아왔으니 선택 취소
+			selected[cnt] = false;
+		}
+		// 현재 재료를 선택하지 않는다
+		selected[cnt] = false;
+		cook(cnt + 1, ban_list);
+	}
+	
+	
+	static Set<Integer> banIngredinets(int ing, Set<Integer> ban_list) {
+		// 현재 식재료와 같이 사용할 수 없는 식재료들
+		Set<Integer> ing_ban_list = ban.get(ing);
+		// 기존 금지 목록에 추가
+		ban_list.addAll(ing_ban_list);
+		return ban_list;
+	}
 }
